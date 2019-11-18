ddb33ed150f1b5044bc404932d8fca5dd34009f4
Ketan <ketan2010bamniya@gmail.com>
2019-11-18 23:47:55 +0530

    added README file for project description

--- api/config/db.js
+++ api/config/db.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
+
+const mongodbUri =
+  process.env.NODE_ENV === "production"
+    ? "mongodb://mongo:27017/security-scan"
+    : "mongodb://localhost:27017/security-scan"
 
 const db = mongoose
-  .connect('mongodb://localhost:27017/security-scan', {
+  .connect(mongodbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log('Database connected...'))
-  .catch(error => console.log('Database Error................', error))
+  .then(() => console.log("Database connected..."))
+  .catch(error => console.log("Database Error................", error))
 
 module.exports = db
